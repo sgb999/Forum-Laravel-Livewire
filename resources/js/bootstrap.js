23c7cc9c0c629f130c9faf56447d12a9fbb85932
Liam <liamag2000@protonmail.com>
2026-06-03 14:15:54 +0100

    feature/27
    Upgrade to Livewire 4

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,7 @@
-window._ = require('lodash');
+window._ = import('lodash');
 
 try {
-    require('bootstrap');
+    import('bootstrap');
 } catch (e) {}
 
 /**
@@ -10,9 +10,9 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = import('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
